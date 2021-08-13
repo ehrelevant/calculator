9e622c7b04acb3d6465628a8d9bdcb474d8b1901
KindaExists <ehrencastillo@gmail.com>
2021-08-13 17:02:43 +0800

    fixed operation functionalities

--- app.js
+++ app.js
@@ -39,26 +39,45 @@ let displayStr = '';
 let storedOperation = [];
 
 function insertValue(evt) {
-    console.log(evt.target.value, displayStr);
+    console.log(evt.target.value);
+
+    const storedLength = storedOperation.length;
     if (evt.target.classList[0] === 'num-btn') {
         displayStr += evt.target.value;
         resultsDis.textContent = displayStr;
-    }
-
-    if (evt.target.classList[0] === 'op-btn') {
-        if (storedOperation.length <= 1) {
-            storedOperation.push(parseInt(displayStr));
-            storedOperation.push(evt.target.value);
-            displayStr = '';
-        } else {
-            const op = storedOperation.pop();
-            const a = storedOperation.pop();
-            const b = parseInt(displayStr);
-            const result = operate(op, a, b);
-            console.log(result);
-            storedOperation.push(result);
+        if (storedLength == 1) {
+            storedOperation.pop();
         }
     }
 
+    console.log(displayStr);
+
+    if (evt.target.classList[0] === 'op-btn') {
+        const op = evt.target.value;
+        if (storedLength == 0) {
+            storedOperation.push(parseInt(displayStr));
+            storedOperation.push(op);
+            displayStr = '';
+        } else if (storedLength == 1) {
+            storedOperation.push(op);
+            displayStr = '';
+        } else if (storedLength == 2 && displayStr == '') {
+            storedOperation[1] = op;
+        } else {
+            const savedOp = storedOperation.pop();
+            const a = storedOperation.pop();
+            const b = parseInt(displayStr);
+
+            const result = operate(savedOp, a, b);
+
+            console.log(result);
+            resultsDis.textContent = result;
+            storedOperation.push(result);
+            if (op !== '=') {
+                storedOperation.push(op);
+            }
+            displayStr = '';
+        }
+    }
     console.log(storedOperation);
 }
